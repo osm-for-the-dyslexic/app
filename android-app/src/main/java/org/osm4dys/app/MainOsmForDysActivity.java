9506ed04be7834b0995297a65601921d080c7b16
develost.com <massimiliano.bernabe@gmail.com>
2016-02-24 13:17:06 +0100

    test web wiev

--- android-app/src/main/java/org/osm4dys/app/MainOsmForDysActivity.java
+++ android-app/src/main/java/org/osm4dys/app/MainOsmForDysActivity.java
@@ -2,13 +2,22 @@ package org.osm4dys.app;
 
 import android.app.*;
 import android.os.*;
+import android.webkit.WebView;
 
 public class MainOsmForDysActivity extends Activity 
 {
+    private WebView mWebView;
+    private static final int MENU_GO_TO_MAP = 1;
+    private static final int MENU_GO_TO_ABOUT = 2;
+    private static final String STARTING_URL = "http://www.osm4dys.org/viewer";
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mWebView = (WebView) findViewById(R.id.mainWebView);
+        mWebView.getSettings().setBuiltInZoomControls(false);
+        mWebView.loadUrl(STARTING_URL);
     }
 }
