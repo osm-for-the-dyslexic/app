657e7e4dbdc3ad212c69ad80aa1be3f0056fcd04
develost.com <massimiliano.bernabe@gmail.com>
2016-02-24 12:43:21 +0100

    test app

---
+++ src/main/java/org/osm4dys/app/MainOsmForDysActivity.java
@@ -0,0 +1,14 @@
+package org.osm4dys.app;
+
+import android.app.*;
+import android.os.*;
+
+public class MainOsmForDysActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
