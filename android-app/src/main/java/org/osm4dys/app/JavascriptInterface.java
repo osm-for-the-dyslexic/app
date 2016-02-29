cc5aa9873d5c4f56705a82de09f3977adf720ccc
develost.com <massimiliano.bernabe@gmail.com>
2016-02-29 12:26:58 +0100

    mod strings and toast

--- android-app/src/main/java/org/osm4dys/app/JavascriptInterface.java
+++ android-app/src/main/java/org/osm4dys/app/JavascriptInterface.java
@@ -2,6 +2,7 @@ package org.osm4dys.app;
 
 import android.speech.tts.TextToSpeech;
 import java.util.Locale;
+import android.widget.Toast;
 
 
 public class JavascriptInterface{
@@ -13,24 +14,26 @@ public class JavascriptInterface{
 	}
 
     public void speak(String textToSay){
-        if (tts != null){
-            tts.speak(textToSay, TextToSpeech.QUEUE_FLUSH, null);
-        }
+        Toast.makeText(mContext, "Speak: " + textToSay, Toast.LENGTH_SHORT).show();
+        //if (tts != null){
+        //    tts.speak(textToSay, TextToSpeech.QUEUE_FLUSH, null);
+        //}
     }
     
     public void setSpeachLanguage(String language){
-        String[] parts = language.split("-");
-        final String l1 = parts[0];
-        final String l2 = parts[1].toUpperCase();
+        Toast.makeText(mContext, "SetSpeachLanguage: " + language, Toast.LENGTH_SHORT).show();
+    //String[] parts = language.split("-");
+        //final String l1 = parts[0];
+        //final String l2 = parts[1].toUpperCase();
 
-        tts = new TextToSpeech(mContext, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if(status != TextToSpeech.ERROR) {
-                    tts.setLanguage(new Locale(l1,l2));
-                }
-            }
-        });
+        //tts = new TextToSpeech(mContext, new TextToSpeech.OnInitListener() {
+        //    @Override
+        //    public void onInit(int status) {
+        //        if(status != TextToSpeech.ERROR) {
+        //            tts.setLanguage(new Locale(l1,l2));
+        //        }
+        //    }
+        //});
     }
     
 }
